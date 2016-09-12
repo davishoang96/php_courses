0441df287229b0fd28cf59cefb953fd4d59f3420
Hoang Viet <davishoang96@gmail.com>
2016-09-12 15:06:44 +0700

    First Commit
    Php easy validation course

---
+++ php_easy_validation/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Easy validator</title>
+  </head>
+  <body>
+    <form class="" action="index.html" method="post">
+      <label for="username">Username</label>
+      <input type="text" name="username" value=""><br>
+      <label for="password">Password</label>
+      <input type="password" name="password" value=""><br>
+      <label for="email">Email</label>
+      <input type="email" name="email" value=""><br>
+      <input type="submit" name="submit" value="submit">
+    </form>
+  </body>
+</html>
